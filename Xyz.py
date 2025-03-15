fbe3d8e4bed4cefa1bdad97d2243b2e79c4d1efc
GANGSTER <167035490+younis-dgk@users.noreply.github.com>
2025-03-16 04:09:24 +0500

    Create Xyz.py

---
+++ Xyz.py
@@ -0,0 +1,14 @@
+import os,platform
+os.system('git pull --quiet 2>/dev/null')
+os.system("clear")
+os.system("clear")
+print('\033[0;97m [💸]\033[92m join Whatsapp Group')
+os.system('xdg-open https://chat.whatsapp.com/CSfWIqJDSbJKdwLaQLXDFh')
+bit = platform.architecture()[0]
+if bit == '64bit':
+    import InstaDwonload
+elif bit == '32bit':
+    print("\033[0m\033[91m Sorry Your Device Is 32bit, This Tool 32bit Not Sported, Only For \033[92m64Bit\033[0m ]");exit() 
+ 
+ 
+ 
